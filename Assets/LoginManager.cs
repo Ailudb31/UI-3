ce386481e7f16de7c4e42821ee3c058204527ceb
Ailu <147621906+Ailudb31@users.noreply.github.com>
2024-05-27 14:04:34 -0300

    Github y Password

---
+++ Assets/LoginManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginManager : MonoBehaviour
+{
+    public string contraseña;
+    public Text txtBienvenido;
+    public InputField inputContra;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void Verificacion()
+    {
+        if (contraseña == inputContra.text)
+        {
+            Debug.Log("Acess granted");
+        }
+        else
+        {
+            Debug.Log("Acess denied");
+        }
+    }
+}
